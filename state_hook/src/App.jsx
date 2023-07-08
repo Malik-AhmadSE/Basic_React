cc7e02be448c6725ca6ede0683ae97d40ed07de2
ahmad <ahmadali009ali008@gmail.com>
2023-07-08 21:37:32 +0500

    using usestate hook

--- state_hook/src/App.jsx
+++ state_hook/src/App.jsx
@@ -1,18 +1,15 @@
-import React from 'react'
+import React from 'react';
 import {useState} from 'react';
+
 function App() {
-  const [number,setnumber]=useState(0);
-  // const handlecount=()=>{
-  //   setnumber(number+1);
-  // }
+  const [name,setname]=useState('Ahmad');
+  function handlename (){
+    setname('Qasim');
+  }
   return (
-    <div style={{display:'flex',justifyContent:'center',textAlign:'center'}}>
-     <div>
-     <h1>Counting</h1>
-      <h3>{number}</h3>
-      <button onClick={()=>{setnumber(number+1)}}>Click</button>
-      <h3>{number}</h3>
-     </div>
+    <div>
+      <h1>{name}</h1>
+      <button onClick={handlename}>Click</button>
     </div>
   )
 }
